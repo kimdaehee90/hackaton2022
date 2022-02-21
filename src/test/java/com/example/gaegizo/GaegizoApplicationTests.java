40f480b144cf3ea86017cb66c69cd9734ba90912
devjaein <devjaein@gmail.com>
2022-02-22 00:54:23 +0900

    conf: project setup

---
+++ src/test/java/com/example/gaegizo/GaegizoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaegizo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaegizoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
