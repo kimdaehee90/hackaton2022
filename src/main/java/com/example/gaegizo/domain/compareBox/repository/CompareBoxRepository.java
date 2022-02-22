e03f4a637fd9b6ad582b715ff3f5e0ee9fdf1ba5
kimdaehee90 <rotnprgl432@gmail.com>
2022-02-22 16:43:18 +0900

    Feat: CompareBox 및 CompareBoxRepository 생성
    공고비교 그룹인 CompareBox와 그 Repository

---
+++ src/main/java/com/example/gaegizo/domain/compareBox/repository/CompareBoxRepository.java
@@ -0,0 +1,8 @@
+package com.example.gaegizo.domain.compareBox.repository;
+
+import com.example.gaegizo.domain.compareBox.domain.CompareBox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompareBoxRepository extends JpaRepository<CompareBox, Long> {
+
+}
